3d72d89cf1a01d34540c300e3952fd7952c656b9
SpeedyRabbit <90570383+SpeedyRabbit@users.noreply.github.com>
2021-10-10 20:07:01 +0000

    front page - 1

---
+++ BootCamp_ACE/labs/lab11/lb11.js
@@ -0,0 +1,49 @@
+// // document.write("<h1>Aloha Planet</h1>");
+// var numKids=5;
+// var partner='David Beckham';
+// var geoLocation='Costa Rica';
+// var jobTitle='web developer';
+
+// var future='You will be a '+jobTitle+' in '+geoLocation+', and married to '+partner+' with ' + numKids +' kids.';
+// document.write(future);
+
+
+// document.write ('This is my first Javascript Page');
+
+// // <form name="alertform">
+// //       Your name:
+// //       <input type="text" name="yourname">
+// //       <input type="button" value="Go" onClick="Window.alert ('Hello '+document.alertform.yourname.value);">
+// // </form>
+
+// x="Hello World!"
+// document.write(x)
+
+// x="IT's MY NAMe...."
+// document.write ("Luna"+x)
+
+// x=prompt ("Enter Number", "");
+// document.write("<br>Luna <br>",+x);
+// x=prompt ("Enter one number: ");
+// y=prompt ("Enter the other number: ");
+// adding=x+y;
+// subtracting=x-y;
+// multiplication=x*y;
+// dividing=x/y;
+// c=confirm("Do you want to continue?")
+// if (c)
+// {
+//   alert ("walk")
+// }
+// else
+// {
+//   alert ("stop")
+// }
+// function test(x){
+//   x=2*x;
+//   console.log(x);
+// }
+// var x=3;
+// console.log(x);
+// test(x);
+// console.log(x);
---
+++ BootCamp_ACE/labs/lab18/lb18.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $("#change2").click(function(){
+        $("div").animate({
+          left:'500px',
+          opacity: '0.5',
+          height:'250px'});
+      });
+    });
